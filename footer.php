1d74ceda2525fc58b9d93b8259f4a898eebab4f7
云落 <sp91@qq.com>
2015-01-30 16:06:55 +0800

    若干修正，若干更新

--- footer.php
+++ footer.php
@@ -2,12 +2,12 @@
 <footer style="border-top: 20px solid ;background-image: url('<?php bloginfo('template_url'); ?>/img/footbg.jpg'); background-repeat: repeat;" class="footer">
     <div class="footer-inner">
         <div class="fooert copyright" align="center">
-         Copyright© 2014-2015  <a href="<?php site_url(); ?>" title="<?php bloginfo(‘name’); ?>"><?php bloginfo(‘name’); ?></a>  |  <a href="<?php site_url(); ?>/sitemap.xml" target="_blank" title="站点地图">站点地图</a>  |  <a href="<?php site_url(); ?>/about" target="_blank" title="关于网站">关于网站</a>  |  <a href="<?php site_url(); ?>/links" target="_blank" title="友情链接">友情链接</a>  |  由 <a rel="nofollow" target="_blank" href="http://googlo.me/go/hengtian">衡天主机</a> & <a rel="nofollow" target="_blank" href="http://googlo.me/go/qiniu">七牛云存储</a> 强力驱动 <span><?php if( dopt('d_track_b') ) echo dopt('d_track'); ?></span>
+         <?php if( dopt('d_footcode_b') ) echo dopt('d_footcode'); ?><span class="trackcode pull-right"><?php if( dopt('d_track_b') ) echo dopt('d_track'); ?></span>
         </div>
     </div>
 </footer>
-<?php if( dopt('d_footcode_b') ) echo dopt('d_footcode'); ?>
 <?php if( dopt('d_fancybox_b') ){?><script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/fancybox.js"></script><?php } ?>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/scrollfollow.js"></script>
 <script type="text/javascript">$(document).ready(function() {$(".fancybox").fancybox();$("#showdiv").fancybox({'centerOnScroll':true});});
 jQuery(document).ready(
 function(jQuery){
