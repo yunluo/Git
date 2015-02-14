93d0d026b999e3cd4a6e0d18cff51e36caec369b
云落 <sp91@qq.com>
2015-02-14 17:02:29 +0800

    修复搜索文章不显示的bug

--- widgets/index.php
+++ widgets/index.php
@@ -8,12 +8,6 @@ include('wid-tags.php');
 include('wid-textbanner.php');
 include('wid-subscribe.php');
 include('wid-slides.php');
-add_action('widgets_init','unregister_d_widget');
-function unregister_d_widget(){
-    unregister_widget('WP_Widget_Search');
-    unregister_widget('WP_Widget_Recent_Comments');
-    unregister_widget('WP_Widget_Tag_Cloud');
-    unregister_widget('WP_Nav_Menu_Widget');
-}
+
 
 ?>
