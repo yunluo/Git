a6a086a35815aa8fb715b9ba7d9c0d155e6b0c8a
云落 <sp91@qq.com>
2015-02-19 20:58:13 +0800

    更换登陆页面获取图片的API

--- functions.php
+++ functions.php
@@ -893,9 +893,10 @@ function custom_headerurl( $url ) {
 add_filter( 'login_headerurl', 'custom_headerurl' );
 
 function custom_login_head(){
-    echo'<style type="text/css">body{background: url(//3.zhucetbid.sinaapp.com);width:100%;height:100%;background-image:url(//3.zhucetbid.sinaapp.com);-moz-background-size: 100% 100%;-o-background-size: 100% 100%;-webkit-background-size: 100% 100%;background-size: 100% 100%;-moz-border-image: url(//3.zhucetbid.sinaapp.com) 0;background-repeat:no-repeat\9;background-image:none\9;}</style>';
+    echo'<style type="text/css">body{background: url(//tu.ihuan.me/api/me_all_pic);width:100%;height:100%;background-image:url(//tu.ihuan.me/api/me_all_pic);-moz-background-size: 100% 100%;-o-background-size: 100% 100%;-webkit-background-size: 100% 100%;background-size: 100% 100%;-moz-border-image: url(//tu.ihuan.me/api/me_all_pic) 0;background-repeat:no-repeat\9;background-image:none\9;}</style>';
 }
 add_action('login_head', 'custom_login_head');
+
 function custom_headertitle ( $title ) {
     return __( '欢迎来到'.get_bloginfo( 'name' ).'' );
 }
