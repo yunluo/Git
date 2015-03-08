170b579fd3db92e81df9dabb8914041a152dd9f3
云落 <sp91@qq.com>
2015-03-08 23:10:09 +0800

    up

--- functions.php
+++ functions.php
@@ -1025,7 +1025,7 @@ function xcollapse($atts, $content = null){
 	return '<div style="margin: 0.5em 0;">
 		<div class="xControl">
 			<span class="xTitle">'.$title.'</span>
-			<a href="javascript:void(0)" class="collapseButton xButton">展开/收缩▼</a>
+			<a href="javascript:void(0)" class="collapseButton xButton"><i class="fa fa-plus-square"></i> 点此展开查看更多内容</a>
 			<div style="clear: both;"></div>
 		</div>
 		<div class="xContent" style="display: none;">'.$content.'</div>
