5ab0a80cee2093dcd46dd4214cf3ff9249b11c4a
云落 <sp91@qq.com>
2015-03-18 00:28:40 +0800

    增加网站地图功能

--- functions.php
+++ functions.php
@@ -122,7 +122,41 @@ if (function_exists('register_sidebar')){
 	));
 }
 
+//页面伪静态
+add_action('init', 'html_page_permalink', -1);
+register_activation_hook(__FILE__, 'active');
+register_deactivation_hook(__FILE__, 'deactive');
 
+function html_page_permalink() {
+	global $wp_rewrite;
+ if ( !strpos($wp_rewrite->get_page_permastruct(), '.html')){
+		$wp_rewrite->page_structure = $wp_rewrite->page_structure . '.html';
+ }
+}
+add_filter('user_trailingslashit', 'no_page_slash',66,2);
+function no_page_slash($string, $type){
+   global $wp_rewrite;
+	if ($wp_rewrite->using_permalinks() && $wp_rewrite->use_trailing_slashes==true && $type == 'page'){
+		return untrailingslashit($string);
+  }else{
+   return $string;
+  }
+}
+
+function active() {
+	global $wp_rewrite;
+	if ( !strpos($wp_rewrite->get_page_permastruct(), '.html')){
+		$wp_rewrite->page_structure = $wp_rewrite->page_structure . '.html';
+ }
+  $wp_rewrite->flush_rules();
+}
+	function deactive() {
+		global $wp_rewrite;
+		$wp_rewrite->page_structure = str_replace(".html","",$wp_rewrite->page_structure);
+		$wp_rewrite->flush_rules();
+}
+
+//面包屑导航
 function deel_breadcrumbs(){
     if( !is_single() ) return false;
     $categorys = get_the_category();
