37da5d7e0d272a196a7c30a0710676e96411d118
云落 <sp91@qq.com>
2015-05-27 14:03:25 +0000

    云落代码提交

--- functions.php
+++ functions.php
@@ -2265,6 +2265,27 @@ function git_comment_add_at( $comment_text, $comment = '') {
   return $comment_text;
 }
 add_filter( 'comment_text' , 'git_comment_add_at', 20, 2);
+/**
+ * 修复 WordPress 找回密码提示“抱歉，该key似乎无效”
+ * http://www.wpdaxue.com/lost-password-error-invalidkey.html
+ */
+function git_reset_password_message($message, $key) {
+    if (strpos($_POST['user_login'], '@')) {
+        $user_data = get_user_by('email', trim($_POST['user_login']));
+    } else {
+        $login = trim($_POST['user_login']);
+        $user_data = get_user_by('login', $login);
+    }
+    $user_login = $user_data->user_login;
+    $msg = __('有人要求重设如下帐号的密码：') . "\r\n\r\n";
+    $msg.= network_site_url() . "\r\n\r\n";
+    $msg.= sprintf(__('用户名：%s') , $user_login) . "\r\n\r\n";
+    $msg.= __('若这不是您本人要求的，请忽略本邮件，一切如常。') . "\r\n\r\n";
+    $msg.= __('要重置您的密码，请打开下面的链接：') . "\r\n\r\n";
+    $msg.= network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_login) , 'login');
+    return $msg;
+}
+add_filter('retrieve_password_message', git_reset_password_message, null, 2);
 //保护后台登录，修改自倡萌：http://www.wpdaxue.com/protected-wp-login.html
 if(git_get_option('git_admin')):
 function git_login_protection() {
