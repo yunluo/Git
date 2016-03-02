61fce34a9c89db7e789419ed9fab3b36352721cf
云落 <sp91@qq.com>
2016-03-02 15:21:53 +0000

    更换更新源

--- inc/theme-options.php
+++ inc/theme-options.php
@@ -1538,7 +1538,7 @@ function git_options_page() {
         
     <?php
                 if (function_exists('curl_init')) {
-            $url = "https://gitcafe.com/googlo/File/raw/master/notice.txt";
+            $url = "https://coding.net/u/googlo/p/File/git/raw/master/notice.txt";
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
