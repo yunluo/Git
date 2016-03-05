740c22c054e88548b727e95fea55a755c9aa85f6
云落 <sp91@qq.com>
2016-03-05 09:59:32 +0000

    提交代码

--- pages/readers.php
+++ pages/readers.php
@@ -9,7 +9,7 @@ function readers_wall( $outer='1',$timer='100',$limit='60' ){
 	$counts = $wpdb->get_results("select count(comment_author) as cnt, comment_author, comment_author_url, comment_author_email from (select * from $wpdb->comments left outer join $wpdb->posts on ($wpdb->posts.id=$wpdb->comments.comment_post_id) where comment_date > date_sub( now(), interval $timer month ) and user_id='0' and comment_author != '".$outer."' and post_password='' and comment_approved='1' and comment_type='') as tempcmt group by comment_author order by cnt desc limit $limit");
 	foreach ($counts as $count) {
 		$c_url = $count->comment_author_url;
-		if (!$c_url) $c_url = 'javascript:;';
+		if (!$c_url) { $c_url = 'javascript:;';}
 		$type .= '<a id="duzhe" target="_blank" href="'. $c_url . '" title="['.$count->comment_author.']近期评论'. $count->cnt . '次">'.get_avatar( $count->comment_author_email, $size = '64' , deel_avatar_default() ).'<span>'.$count->comment_author.'</span></a>';
 	}
 	echo $type;
