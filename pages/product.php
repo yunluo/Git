c9930e590ce0289cf2d4bcd38cb2abc1cc7b53ab
云落 <sp91@qq.com>
2016-03-17 06:55:48 +0000

    提交代码

--- pages/product.php
+++ pages/product.php
@@ -27,7 +27,16 @@ get_header(); ?>
         the_permalink(); ?>" alt="<?php
         the_title(); ?>" title="<?php
         the_title(); ?>" class="fancyimg home-blog-entry-thumb">
-					<div class="thumb-img focus"><?php
+					<div class="thumb-img focus">
+					<div class="metacat"><?php
+    if (!is_category()) {
+        $category = get_the_category();
+        if ($category[0]) {
+            echo '<a class="metacat" href="' . get_category_link($category[0]->term_id) . '">' . $category[0]->cat_name . '</a>';
+        }
+    }; ?>
+</div>
+					<?php
             echo '<img class="thumb" title="'.get_the_title().'" src="' . get_bloginfo("template_url") . '/timthumb.php?src=';
             echo post_thumbnail_src();
             echo '&h=375&w=375&q=90&zc=1&ct=1" width="375px" height="375px" alt="' . get_the_title() . '" />';
