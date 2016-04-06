db1bef2e4b3920fd0511782ed5143452851297e7
云落 <sp91@qq.com>
2016-04-06 14:05:55 +0000

    修改打算的样式

--- single.php
+++ single.php
@@ -124,7 +124,7 @@ while (have_posts()):
     } else {
         echo '0';
     } ?></span>)</a><?php
-    if (git_get_option('git_bdshare_b')) echo "<span class='or'><style>.article-social a:hover{background:#fff;}</style><a class='shangqr' style='border-bottom:0px;font-size:15pt;' id='showdiv' href='#donatecoffee'>赏</a></span>";
+    if (git_get_option('git_bdshare_b')) echo '<span class="or"><style>.article-social a:hover{background:#fff;}</style><a class="weixin" style="border-bottom:0px;font-size:15pt;cursor:pointer;">赏<div class="weixin-popover"><div class="popover bottom in"><div class="arrow"></div><div class="popover-title"><center>支付宝[' . git_get_option('git_pay') . ']</center></div><div class="popover-content"><img width="200px" height="200px" src="' . git_get_option('git_pay_qr') . '" ></div></div></div></a></span>';
     deel_share(); ?>
 </div>
 	</article>
