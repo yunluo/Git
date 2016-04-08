a3654725fc1aab5060ba9f5b49bf7d9905d6588a
云落 <sp91@qq.com>
2016-04-08 15:51:05 +0000

    tag图标描述优化

--- tag.php
+++ tag.php
@@ -2,7 +2,11 @@
 <div class="content-wrap">
 	<div class="content">
 		<header class="archive-header">
+			<?php if(tag_description()){;?>
+			<div class="archive-header-banner"><?php echo tag_description(); ?></div>
+			<?php }else{;?>
 			<h1>标签：<?php echo single_tag_title(); ?></h1>
+			<?php };?>
 		</header>
 		<?php if (git_get_option('git_card_b'))
 		{
