f6acc703ab41d65797ef25a44237125f7d830e81
云落 <sp91@qq.com>
2016-04-13 15:19:07 +0000

    完善网易云音乐功能

--- functions.php
+++ functions.php
@@ -1284,7 +1284,7 @@ add_shortcode('dltable', 'xdltable');
 //网易云音乐
 function music163($atts) {
     extract(shortcode_atts(array("id" => "" ) , $atts));
-    return '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=100% height=86 src="http://music.163.com/outchain/player?type=2&id=' . $id . '&auto=1&height=66"></iframe>';
+    return '<iframe style="width:100%;max-height:86px;" frameborder="no" border="0" marginwidth="0" marginheight="0" src="http://music.163.com/outchain/player?type=2&id=' . $id . '&auto=1&height=66"></iframe>';
 }
 add_shortcode('netmusic', 'music163');
 // add youku using iframe
