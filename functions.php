fc17a4e9e606c34b825ad6453a72e5d2404dae83
云落 <sp91@qq.com>
2016-05-28 12:27:31 +0000

    小小优化一下

--- functions.php
+++ functions.php
@@ -9,7 +9,8 @@ function deel_setup() {
     remove_action('wp_head', 'rsd_link');
     remove_action('wp_head', 'wlwmanifest_link');
     remove_action('wp_head', 'index_rel_link');
-    remove_action('wp_head', 'start_post_rel_link', 10, 0);
+    remove_action('wp_head', 'start_post_rel_link');
+	remove_action('wp_head', 'adjacent_posts_rel_link');
     remove_action('wp_head', 'wp_generator');
     add_action('widgets_init','unregister_d_widget');
     function unregister_d_widget(){
