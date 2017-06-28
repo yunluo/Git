148c62dc5fe8ed03c97721e4586f9d1d33ddc2ef
云落 <sp91@qq.com>
2017-06-28 03:05:28 +0000

    试试哈

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="Cache-Control" content="no-transform" />
 <meta http-equiv="Cache-Control" content="no-siteapp" />
-<meta http-equiv="X-UA-Compatible" content="IE=10,IE=9,IE=8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,IE=10,IE=9,IE=8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 <link href="/favicon.ico" rel="icon" type="image/x-icon" />
 <?php
