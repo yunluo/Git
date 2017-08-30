833d70d41ad27527b597b18d462e6fd9d6dd6bb2
云落 <sp91@qq.com>
2017-08-30 10:12:47 +0000

    增加侧边栏文件缓存

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,18 @@
+<?php 
+$sidebar_html = ABSPATH . "wp-content/cache/sidebar.txt";
+$have_cached = false;
+if (file_exists($sidebar_html)){
+    $file_time = filemtime($sidebar_html);
+    if (($file_time + 18000) > time()){ //缓存5小时
+        echo "<!-- cached sidebar -->";
+        echo(file_get_contents($sidebar_html));
+        echo "<!-- end of cached sidebar -->";
+        $have_cached = true;
+    }
+}
+if(!$have_cached){
+    ob_start();
+?>
 <aside class="sidebar">
 <?php
 if (function_exists('dynamic_sidebar') && dynamic_sidebar('widget_sitesidebar')) : endif;
@@ -16,3 +31,16 @@ else {
 }
 ?>
 </aside>
+<?php
+    $sidebar_content = ob_get_contents();
+    ob_end_clean();
+    $sidebar_fp = fopen($sidebar_html, "w");
+ 
+    if ($sidebar_fp){
+         fwrite($sidebar_fp, $sidebar_content);
+         fclose($sidebar_fp);
+    }
+ 
+    echo $sidebar_content;
+}
+?>
