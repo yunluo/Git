17570dc36a001f0c2f81896851783549dbd6c0d3
云落 <sp91@qq.com>
2017-09-17 07:34:08 +0000

    修复错字

--- header.php
+++ header.php
@@ -235,7 +235,7 @@ if (git_get_option('git_sign_b')) {
 		<?php if(is_user_logged_in()){
 		    echo '<i class="fa fa-user"></i> <a href="' . home_url() . '/wp-admin">' . $u_name . '</a>';
 		}elseif(defined('UM_DIR')){
-			echo '<i class="fa fa-power-off"></i>  <a styliah="cursor:pointer;" data-sign="0" class="user-login">点击登录</a>';}
+			echo '<i class="fa fa-power-off"></i>  <a style="cursor:pointer;" data-sign="0" class="user-login">点击登录</a>';}
 		else{
 		    echo '<i class="fa fa-power-off"></i> <a id="showdiv" href="#loginbox" data-original-title="点击登录">点击登录</a>';}
 		    ?>
