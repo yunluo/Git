3241f1f22091e613da7721dfda37d64bfc5f2f20
云落 <sp91@qq.com>
2017-12-02 02:20:57 +0000

    修复本地jQuery乱码的问题，优化3D幻灯片js的加载方式，换成有兼容性更好的方式

--- modules/sticky.php
+++ modules/sticky.php
@@ -66,5 +66,4 @@ if (git_get_option('git_qncdn_b')) {
 ?>
 </div>
 </div><div class="ws_shadow"></div>
-	</div><script type="text/javascript" src="<?php
-echo esc_url( get_template_directory_uri() ); ?>/assets/js/slider.js"></script>
+	</div><?php wp_enqueue_script('slider', get_stylesheet_directory_uri() . '/assets/js/slider.js', array('jquery')); ?>
