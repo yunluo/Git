7e728ca3c99b1423cd4d98ceecd551fb5430106f
云落 <sp91@qq.com>
2019-02-01 04:35:25 +0000

    提交更新

--- include/func_load.php
+++ include/func_load.php
@@ -4,7 +4,7 @@ require ( 'shortcode.php' );//短代码
 
 require ( 'optimization.php' );//优化措施
 
-require ( 'avatar.php' );//头像
+require ( 'avatar.php' );//头像功能
 
 require ( 'server.php' );//第三方服务
 
@@ -12,4 +12,4 @@ require ( 'user.php' );//用户服务
 
 require ( 'seo.php' );//seo功能
 
-require ( 'email.php' );//邮箱服务
+require ( 'email.php' );//邮箱功能
