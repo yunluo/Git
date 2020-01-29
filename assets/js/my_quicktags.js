7134baf35e5e1094f0dc84160bc9c0e8f7cfea12
云落 <sp91@qq.com>
2020-01-29 13:26:10 +0800

    修改段代码按钮

--- assets/js/my_quicktags.js
+++ assets/js/my_quicktags.js
@@ -42,6 +42,6 @@ QTags.addButton( 'phpcode', '运行PHP', '[phpcode file="文件名"]', "" );
 QTags.addButton( 'nl', '文章内链', '[neilian ids=]', '');
 QTags.addButton( 'wl', '文章外链', '[wailian]', '[/wailian]');
 QTags.addButton( 'ulli', '无序列表', '[list]', '[/list]' );
-QTags.addButton( 'pay', '会员付费查看', '[pay point="10"]', '[/pay]' );
-QTags.addButton( 'paxx', '游客付费查看', '[paxx money=2]', '[/pax]' );
+QTags.addButton( 'pay', '会员付费查看', '[pay point="10"]\n这里是需要付费的内容\n[/pay]', '' );
+QTags.addButton( 'pax', '游客付费查看', '[paxx money=1]\n这里是需要付费的内容\n[/pax]', '' );
 //这儿共有四对引号，分别是按钮的ID、显示名、点一下输入内容、再点一下关闭内容（此为空则一次输入全部内容），\n表示换行。
